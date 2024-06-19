2471f03637d8c3f34841a533b2f0876473f13567
Bartolomeu Hangalo <bartolomeuhangalo1@gmail.com>
2024-06-19 07:40:41 +0100

    Design nav bar

--- src/main/java/dev/hangalito/topics/config/WebConfig.java
+++ src/main/java/dev/hangalito/topics/config/WebConfig.java
@@ -29,5 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
     }
+
 }
